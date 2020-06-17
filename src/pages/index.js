59506d5025879b1ca43bff48b9dd76e7d51cf271
Biuni <info.gianlucabonifazi@gmail.com>
2020-06-17 18:27:51 +0200

    Update 17-06-2020

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ export default () => (
     query={graphql`
       query AllValue {
         allRtCsv(
-          filter: { date: { gt: "2020-06-16" } }
+          filter: { date: { gt: "2020-06-17" } }
           sort: { fields: ML, order: DESC }
         ) {
           totalCount

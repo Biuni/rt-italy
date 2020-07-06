bc681d6e13c7a0b0f8bb28111b03258a316708a9
Biuni <info.gianlucabonifazi@gmail.com>
2020-07-06 10:41:24 +0200

    Update 06-07-2020

--- src/components/Information.js
+++ src/components/Information.js
@@ -87,9 +87,9 @@ function Information(props) {
           Per adattarlo ai dati italiani, come consigliato dall'
           <strong>Istituto Superiore della Sanità</strong>, sono considerati i
           casi negli ultimi 15 giorni invece che 7, come indicato da Systrom.
-          <h3>Aggiornamento dei dati</h3>Dal 26-06-2020, il valore di R<sub>t</sub>{" "}
+          <h3>Aggiornamento dei dati</h3>Dal 06-07-2020, il valore di R<sub>t</sub>{" "}
           (ed i rispettivi intervalli di credibilità) non verranno più aggiornati
-          quotidianamente ma ogni 3 giorni. Questa scelta deriva dalla poca variabilità
+          quotidianamente ma ogni Lunedì. Questa scelta deriva dalla poca variabilità
           dei casi e dal problema del continuo ricalcolo che le regioni effettuano
           oramai ogni giorno, rendendo impossibile effettuare una predizione credibile.
           <h3>Disclaimer</h3>

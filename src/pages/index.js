497cd35c769e18ed9c7eae07c9e80cf5b5175b17
Biuni <info.gianlucabonifazi@gmail.com>
2021-02-26 15:00:53 +0100

    Update ISS data 26-02-2021

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,7 +78,7 @@ export default () => (
     query={graphql`
       query AllValue {
         allRtCsv(
-          filter: { date: { gt: "2021-02-03" } }
+          filter: { date: { gt: "2021-02-10" } }
           sort: { fields: ML, order: DESC }
         ) {
           totalCount
